075c31bfbd277a419bd25d4976c9883908f9a91b
somrat <monnaf37@gmail.com>
2020-04-05 12:48:34 +0600

    modified search style

--- assets/js/script.js
+++ assets/js/script.js
@@ -113,4 +113,18 @@
     tabPane.addClass('active');
   });
 
+
+
+  // search
+  $('#search').keyup(function () {
+    if (this.value) {
+      $(this).addClass('active')
+    } else {
+      $(this).removeClass('active')
+    }
+  })
+  $('#search').focusout(function () {
+    $(this).removeClass('active')
+  })
+
 })(jQuery);
